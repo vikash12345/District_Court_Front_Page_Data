cbf13af7b3d7a0db8000bc2254561de2fb63f9e6
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-11-06 14:57:31 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,27 +1,46 @@
 <?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
-
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
-
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
+require 		'scraperwiki.php';
+require 		'scraperwiki/simple_html_dom.php';
+$BaseLink	=	'http://202.61.43.40:8080/';
+	$SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=0';
+	sleep(5);
+	$Pagination = 	file_get_html($SiteURL);
+	$numberforloop = $Pagination->find("//*[@id='w0']/div/b[2]",0)->plaintext;
+	$text = str_replace(',', '', $numberforloop);
+//	$loop = $text/20;
+$loop = 3;
+	
+	
+	// echo $AllPages = (int)$numberforloop;
+	
+	
+	
+	//	Page pagination
+	for($PageLoop = 1; $PageLoop < $loop; $PageLoop++){
+	$FinalURL  		=  'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page='.$PageLoop;
+		$Html		=	file_get_html($FinalURL);
+		sleep(10);
+		$RowNumb	=	-1;
+		echo "Page No = > " . "$PageLoop.\n";
+		if ($Html) {
+			//	Paginate all 'View' buttons
+			foreach ($Html->find("//div[@id='w0']/table[contains(@class,'table-striped')]/tbody/tr") as $element) {
+				$RowNumb	+=	1;
+				if ($RowNumb != 0) {
+					sleep(2);
+					$CourtName	=	$element->find('./td[2]', 0);
+					
+					
+					$CaseNumbr	=	$element->find('./td[3]', 0);
+					$CaseStats	=	$element->find('./td[4]', 0);
+					$CaseValue	=	$element->find('./td[5]/button', 0);
+					$CaseLinkR	=	$BaseLink . $CaseValue->attr['value'];
+					$CaseLink	=	str_replace("amp;", "", $CaseLinkR);
+          echo "$CaseLink\n"
+					
+	 }
+	}}
+	
+	}	
+			
 ?>
