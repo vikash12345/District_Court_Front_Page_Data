a74afb8afa387f9fbc5ae35493c4a0486563b13a
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-11-13 11:37:14 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,6 +1,7 @@
 <?
 require 		'scraperwiki.php';
 require 		'scraperwiki/simple_html_dom.php';
+error_reporting(0);
 $BaseLink	=	'http://202.61.43.40:8080/';
 	$SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyfir&page=0';
 	sleep(5);
