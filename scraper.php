d8a611dc49d8d69afa33f115a73c6fc7a17cb795
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-02 15:38:06 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -19,7 +19,7 @@ $BaseLink	=	'http://202.61.43.40:8080/';
 	for($PageLoop = 1; $PageLoop < $loop; $PageLoop++)
 	{
 		
-		  		=  'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyfir&page='.$PageLoop;
+		$FinalURL	=  'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyfir&page='.$PageLoop;
 		echo "URL  - > 	" . "$FinalURL.\n";
 		$Html		=	file_get_html($FinalURL);
 		sleep(10);
